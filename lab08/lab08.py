639d448064998559798b88b0f32e6261ba1195e4
沈陽明 (Steven Shen) <116272268+Bonbodii@users.noreply.github.com>
2023-12-05 17:44:59 +0800

    Add files via upload

--- lab08/lab08.py
+++ lab08/lab08.py
@@ -1,49 +1,94 @@
 import cv2
-import dlib
 import numpy as np
-from math import sqrt
 
-def dis(tvec):
-    return round(float(tvec[2]), 2)
+# Known parameters
+known_person_width = 0.5  # Width of the person in meters (example value)
+known_person_height = 1.7  # Height of the person in meters (example value)
+known_face_width = 0.15   # Width of the face in meters (example value)
+known_face_height = 0.2   # Height of the face in meters (example value)
+focal_length = 25000       # Focal length of the camera (example value)
 
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+# Chessboard pattern size for calibration
+pattern_size = (6, 9)
 
-detector = dlib.get_frontal_face_detector()
+# Arrays to store object points and image points from all the images
+objpoints = []  # 3D points in real-world space
+imgpoints = []  # 2D points in the image plane
+calibrated = False
 
-cap = cv2.VideoCapture(0)
-f = cv2.FileStorage("calibrate.xml", cv2.FILE_STORAGE_READ)
-intrinsic = f.getNode("intrinsic").mat()
-distortion = f.getNode("distortion").mat()
+# Camera calibration parameters
+camera_matrix = None
+dist_coeffs = None
 
-face_x = 14.5
-# body_x = 75
-body_y = 200
-face_objPoints = np.float32([(0, 0, 0), (face_x, 0, 0), (face_x, face_x, 0), (0, face_x, 0)])
+# Generate object points for the chessboard
+objp = np.zeros((np.prod(pattern_size), 3), dtype=np.float32)
+objp[:, :2] = np.mgrid[0:pattern_size[0], 0:pattern_size[1]].T.reshape(-1, 2)
+
+# Open the camera
+print("Turning on camera")
+cap = cv2.VideoCapture(0)  # 0 represents the default camera, change as needed
+print("Starting reading.")
 
 while True:
-    _, src = cap.read()
-    rects, weights = hog.detectMultiScale(src, winStride=(8, 8), scale=1.05, useMeanshiftGrouping = False)
-    face_rects = detector(src, 0)
-    for i, d in enumerate(face_rects):
-        x1 = d.left()
-        y1 = d.top()
-        x2 = d.right()
-        y2 = d.bottom()
-        # print(abs(x1-x2), abs(y1-y2))
-        src = cv2.rectangle(src, (x1, y1), (x2, y2), (0, 0, 255), 2)
-        imgPoints = np.float32([(x1, y1), (x2, y1), (x2, y2), (x1, y2)])
-        _, _, tvec = cv2.solvePnP(face_objPoints, imgPoints, intrinsic, distortion)
-        cv2.putText(src, str(dis(tvec)), (x2, y2), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 1, cv2.LINE_AA)
-    for rect in rects:
-        (x, y, w, h) = rect
-        src = cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        imgPoints = np.float32([(x, y), (x+w, y), (x+w, y+h), (x, y+h)])
-        body_x = body_y / 2
-        print(body_x, body_y)
-        body_objPoints = np.float32([(0, 0, 0), (body_x, 0, 0), (body_x, body_y, 0), (0, body_y, 0)])
-        _, _, tvec = cv2.solvePnP(body_objPoints, imgPoints, intrinsic, distortion)
-        print(tvec)
-        cv2.putText(src, str(dis(tvec)), (x+w, y+h), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.imshow("t", src)
-    cv2.waitKey(1)
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    if not calibrated:
+        # Find chessboard corners
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, pattern_size, None)
+
+        if ret:
+            cv2.drawChessboardCorners(frame, pattern_size, corners, ret)
+            objpoints.append(objp)
+            imgpoints.append(corners)
+
+            # Calibrate camera using the collected points
+            ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
+                objpoints, imgpoints, gray.shape[::-1], None, None
+            )
+
+            calibrated = True
+
+    else:
+        # Undistort the frame using cameraMatrix and distCoeffs
+        undistorted_frame = cv2.undistort(frame, camera_matrix, dist_coeffs)
+
+        # initialize the HOG descriptor/person detector
+        hog = cv2.HOGDescriptor()
+        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+        rects, weights = hog.detectMultiScale(
+            undistorted_frame, winStride=(6, 6), padding=(8, 8), scale=1.05, useMeanshiftGrouping=False
+        )
+        #focal length
+        #focal_length = camera_matrix[0, 0]
+
+        # draw the bounding boxes for people, yellow
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 3)
+
+            # Calculate depth for people
+            depth = (known_person_height * focal_length) / (h*100)
+            cv2.putText(frame, f'Depth: {depth:.2f} meters', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        rects = face_cascade.detectMultiScale(undistorted_frame, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+        # draw the bounding boxes for faces, green
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+            # Calculate depth for faces
+            depth = (known_face_height * focal_length) / (h*100)
+            cv2.putText(frame, f'Depth: {depth:.2f} meters', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow('Calibration', frame)
+
+    # Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
